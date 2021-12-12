b7711b9d2f27765e730c306931975d614633a5b7
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2021-12-12 15:30:49 +0900

    Remove jquery, instead use fetch API

--- client/src/utils/fetchers.js
+++ client/src/utils/fetchers.js
@@ -5,14 +5,11 @@ import { gzip } from 'pako';
  * @returns {Promise<ArrayBuffer>}
  */
 async function fetchBinary(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'binary',
-    method: 'GET',
-    responseType: 'arraybuffer',
-    url,
-  });
-  return result;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${res.url}`);
+  }
+  return res.arrayBuffer();
 }
 
 /**
@@ -21,13 +18,11 @@ async function fetchBinary(url) {
  * @returns {Promise<T>}
  */
 async function fetchJSON(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'json',
-    method: 'GET',
-    url,
-  });
-  return result;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${res.url}`);
+  }
+  return res.json();
 }
 
 /**
@@ -37,18 +32,15 @@ async function fetchJSON(url) {
  * @returns {Promise<T>}
  */
 async function sendFile(url, file) {
-  const result = await $.ajax({
-    async: false,
-    data: file,
-    dataType: 'json',
+  const res = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/octet-stream',
+      Accept: 'application/json',
     },
-    method: 'POST',
-    processData: false,
-    url,
+    body: file,
   });
-  return result;
+  return res.json();
 }
 
 /**
@@ -57,24 +49,31 @@ async function sendFile(url, file) {
  * @param {object} data
  * @returns {Promise<T>}
  */
+// async function sendJSON(url, data) {
+//   const res = fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Accept: 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   });
+//   return res.json();
+// }
 async function sendJSON(url, data) {
   const jsonString = JSON.stringify(data);
   const uint8Array = new TextEncoder().encode(jsonString);
   const compressed = gzip(uint8Array);
 
-  const result = await $.ajax({
-    async: false,
-    data: compressed,
-    dataType: 'json',
+  const res = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Encoding': 'gzip',
       'Content-Type': 'application/json',
     },
-    method: 'POST',
-    processData: false,
-    url,
+    body: compressed,
   });
-  return result;
+  return res.json();
 }
 
 export { fetchBinary, fetchJSON, sendFile, sendJSON };
